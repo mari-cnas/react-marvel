45b5081dc5d95698cba6883fba5075b5a91d7a91
Mariana Carvalho Nascimento <contatemariana@gmail.com>
2022-10-31 21:34:35 -0300

    error comic card

--- src/components/ComicCard/styled.ts
+++ src/components/ComicCard/styled.ts
@@ -1,9 +1,9 @@
-import { Card, Ratio, Row } from 'react-bootstrap';
+import { Ratio } from 'react-bootstrap';
 import styled from 'styled-components';
 
 interface ICoverProps {
   coverimage: string;
-  aspectRatio: string;
+  aspectRatio: number;
 }
 
 export const ColoredCard = styled.div`
@@ -15,4 +15,5 @@ export const ComicImage = styled(Ratio)<ICoverProps>`
   background-position: center center;
   background-repeat: no-repeat;
   box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  aspect-ratio: 150;
 `;
